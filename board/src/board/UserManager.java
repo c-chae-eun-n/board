e3e8c32137db0bf0bcf133f511a7e2667531dab2
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-28 18:13:23 +0900

    Modify : UserManager class & Add constructor

--- board/src/board/UserManager.java
+++ board/src/board/UserManager.java
@@ -1,6 +1,18 @@
 package board;
 
-public class UserManager {
+import java.util.ArrayList;
 
+public class UserManager {
+	private ArrayList<User> userList;
+	
+	private UserManager() {
+		userList = new ArrayList<>();
+	}
+	
+	private static UserManager instance = new UserManager(); 
+	
+	public static UserManager getInstance() {
+		return instance;
+	}
 
 }
