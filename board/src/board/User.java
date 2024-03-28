9032e8ff8b089c640f50b0192337647794167f86
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-28 18:15:56 +0900

    Add : User constructor

--- board/src/board/User.java
+++ board/src/board/User.java
@@ -2,4 +2,13 @@ package board;
 
 public class User {
 	String id, password;
+	
+	public User() {
+		
+	}
+	
+	public User(String id, String password) {
+		this.id = id;
+		this.password = password;
+	}
 }
