50e06a617b1d28e30b292e303c58fb965ff33fa1
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-28 18:02:48 +0900

    Modify : Main class

--- board/src/board/Main.java
+++ board/src/board/Main.java
@@ -3,8 +3,9 @@ package board;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System system = new System();
+		system.run();
+		
 	}
 
 }
