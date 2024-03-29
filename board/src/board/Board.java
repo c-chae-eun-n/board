b12ffba96d824dd762a177e5aaa3e3e2d5a980f2
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-29 14:57:12 +0900

    Add : constructor

--- board/src/board/Board.java
+++ board/src/board/Board.java
@@ -1,12 +1,12 @@
 package board;
 
 public class Board {
-	private int count = 0;							// 전체 게시글 수
-	private int pageSize = 5;						// 한 페이지에 보여줄 게시글 수
-	private int curPageNum = 1;						// 현재 페이지 번호
-	private int pageCount = 1;						// 전체 페이지 개수
-	private int startRow = 0;						// 현재 페이지의 게시글 시작 번호
-	private int endRow = 0;							// 현재 페이지의 게시글 마지막 번호
+	String title;
+	String body;
 	
+	public Board(String title, String body) {
+		this.title = title;
+		this.body = body;
+	}
 	
 }
