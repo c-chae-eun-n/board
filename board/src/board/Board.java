b546fc29308b86f5f928873f4fbf5afa7dac00ef
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-29 14:59:34 +0900

    Add : title & body getter,setter

--- board/src/board/Board.java
+++ board/src/board/Board.java
@@ -9,4 +9,19 @@ public class Board {
 		this.body = body;
 	}
 	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getBody() {
+		return this.body;
+	}
+	
+	public void setBody(String body) {
+		this.body = body;
+	}
 }
