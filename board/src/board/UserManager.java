02a143c8bfe741f837ab91c01db5a7349e138917
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-29 16:44:47 +0900

    delete : UserManager All

--- board/src/board/UserManager.java
+++ board/src/board/UserManager.java
@@ -1,78 +1,32 @@
 package board;
 
-import java.util.ArrayList;
 
-public class UserManager {
-	private ArrayList<User> userList;
-	
-	private UserManager() {
-		userList = new ArrayList<>();
-	}
-	
-	private static UserManager instance = new UserManager(); 
-	
-	public static UserManager getInstance() {
-		return instance;
-	}
+public class UserManager implements CRUD {
 
-	// Create
-	public User createUser(String id, String password) {
-		if(isValidId(id)) {
-			User user = new User(id, password);
-			userList.add(user);
-			return user.clone();
-		}
-		return new User();
-	}
-	
-	private boolean isValidId(String id) {
-		User user = findUserById(id);
-		if(user.getId() == null)
-			return true;
-		return false;
-	}
-	
-	public User findUserById(String id) {
-		for(User user : userList) {
-			if(user.getId().equals(id))
-				return user;
-		}
-		return new User();
-	}
-	
-	public int findUserindexById(String id) {
-		for(int i=0; i<userList.size(); i++) {
-			User user = userList.get(i);
-			if(user.getId().equals(id))
-				return i;
-		}
-		return -1;
-	}
-	
-	// Read
-	public User getUser(int index) {
-		User user = userList.get(index);
-		return user;
-	}
-	
-	// Delete 
-	public void removeUser(int index) {
-		if(index < 0 || index >= userList.size()) {
-			System.err.println("유효하지 않은 범위입니다.");
-			return;
-		}
+	@Override
+	public void crate(User user) {
+		// TODO Auto-generated method stub
 		
-		userList.remove(index);
 	}
+
+	@Override
+	public void read(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
-	public int userSize() {
-		return userList.size();
-	}
-	
-	// 검수용
-	public void printUserAll() {
-		for(User user : userList)
-			System.out.println(user);
-	}
 	
 }
