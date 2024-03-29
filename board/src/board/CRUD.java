39d7d781bd6a5a99bd3fce571d33409a8a3e23f3
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-29 17:05:13 +0900

    Update : crate -> create (오타수정)

--- board/src/board/CRUD.java
+++ board/src/board/CRUD.java
@@ -1,7 +1,7 @@
 package board;
 
 public interface CRUD {
-	public void crate(User user);
+	public void create(User user);
 	public void read(User user);
 	public void update(User user);
 	public void delete(User user);
