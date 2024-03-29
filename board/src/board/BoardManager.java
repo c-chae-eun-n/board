7a0644066492d06e5f83ef98e57122ee049f8f9b
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-29 16:50:43 +0900

    Update : BoardManager

--- board/src/board/BoardManager.java
+++ board/src/board/BoardManager.java
@@ -1,5 +1,37 @@
 package board;
 
-public class BoardManager {
+import java.util.ArrayList;
 
+public class BoardManager implements CRUD {
+	private ArrayList<Board> boardList;
+
+	public BoardManager() {
+		boardList = new ArrayList<>();
+	}
+
+	@Override
+	public void crate(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void read(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
