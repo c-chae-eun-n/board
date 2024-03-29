e7bdb3cbfa18918cbb3eacfc8e3680c186062336
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-29 10:59:33 +0900

    Style: Additions related to styling only

--- board/src/board/BoardSystem.java
+++ board/src/board/BoardSystem.java
@@ -26,6 +26,8 @@ public class BoardSystem {
 	}
 	
 	private void printMainMenu() {
+		System.out.println("=== 콘솔 게시판 ===");
+		System.out.println("================");
 		System.out.println("[1]회원가입");
 		System.out.println("[2]회원탈퇴");
 		System.out.println("[3]로그인");
@@ -34,6 +36,7 @@ public class BoardSystem {
 		System.out.println("[6]게시물 삭제");
 		System.out.println("[7]게시물 조회");
 		System.out.println("[0]종료");
+		System.out.println("================");
 	}
 	
 	private int inputNumber(String message) {
