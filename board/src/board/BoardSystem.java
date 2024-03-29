0c42d34038877598cba7b3c7b14674bf335773b0
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-29 10:33:58 +0900

    Add : printMainMenu 클래스 추가 & constant EXIT 추가

--- board/src/board/BoardSystem.java
+++ board/src/board/BoardSystem.java
@@ -9,6 +9,7 @@ public class BoardSystem {
 	private final int POST = 5;
 	private final int DELETE = 6;
 	private final int VIEW = 7;
+	private final int EXIT = 0;
 	
 	private UserManager userManager = UserManager.getInstance();
 	
@@ -18,6 +19,16 @@ public class BoardSystem {
 		log = -1;
 	}
 	
+	private void printMainMenu() {
+		System.out.println("[1]회원가입");
+		System.out.println("[2]회원탈퇴");
+		System.out.println("[3]로그인");
+		System.out.println("[4]로그아웃");
+		System.out.println("[5]게시물 작성");
+		System.out.println("[6]게시물 삭제");
+		System.out.println("[7]게시물 조회");
+		System.out.println("[0]종료");
+	}
 	
 	public void run() {
 		
