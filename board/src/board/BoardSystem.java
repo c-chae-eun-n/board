cc11817f5a61f926dc1c8611668c58f29318af76
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-29 10:30:30 +0900

    Add : constant

--- board/src/board/BoardSystem.java
+++ board/src/board/BoardSystem.java
@@ -2,6 +2,17 @@ package board;
 
 public class BoardSystem {
 	
+	private final int JOIN = 1;
+	private final int LEAVE = 2;
+	private final int LOGIN = 3;
+	private final int LOGOUT = 4;
+	private final int POST = 5;
+	private final int DELETE = 6;
+	private final int VIEW = 7;
+
+	
+	
+	
 	public void run() {
 		
 	}
